002ca0074925749dd5be93eb456e2129aaad71cd
Elliot J. Voris <elliot@voris.me>
2015-04-28 15:22:57 -0500

    Issue #2: Playing with submitting Google spreadsheet data via ajax
    Hopefully this will pan out so that we can use the site to collect
    the registration information and send them to PayPal to pay.
    
    That would be great!
    
    Signed-off-by: Elliot J. Voris <elliot@voris.me>

---
+++ js/register_me.js
@@ -0,0 +1,68 @@
+$(function() {
+
+    $("input,textarea").jqBootstrapValidation({
+        preventSubmit: true,
+        submitError: function($form, event, errors) {
+            // additional error messages or events
+        },
+        submitSuccess: function($form, event) {
+            event.preventDefault(); // prevent default submit behaviour
+            // get values from FORM
+            var name = $("input#reg-name").val();
+            var email = $("input#reg-email").val();
+            var url = $("input#reg-url").val();
+            var firstName = name; // For Success/Failure Message
+            // Check for white space in name for Success/Fail message
+            if (firstName.indexOf(' ') >= 0) {
+                firstName = name.split(' ').slice(0, -1).join(' ');
+            }
+            $.ajax({
+                url: "https://docs.google.com/a/voris.me/forms/d/1URNV6647mn4yDhsgAm3Yvg3mG4FE8Oxgrt2emS_prZA/formResponse",
+                type: "POST",
+                data: {
+                    "entry.1239589934": name,
+                    "entry.1275507538": email,
+                    "entry.926689343": url
+                },
+                dataType: "xml",
+                success: function() {
+                    // Success message
+                    $('#success').html("<div class='alert alert-success'>");
+                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-success')
+                        .append("<strong>Your message has been sent. </strong>");
+                    $('#success > .alert-success')
+                        .append('</div>');
+
+                    //clear all fields
+                    $('#registerForm').trigger("reset");
+                },
+                error: function() {
+                    // Fail message
+                    $('#success').html("<div class='alert alert-danger'>");
+                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    $('#success > .alert-danger').append('</div>');
+                    //clear all fields
+                    $('#registerForm').trigger("reset");
+                },
+            })
+        },
+        filter: function() {
+            return $(this).is(":visible");
+        },
+    });
+
+    $("a[data-toggle=\"tab\"]").click(function(e) {
+        e.preventDefault();
+        $(this).tab("show");
+    });
+});
+
+
+/*When clicking on Full hide fail/success boxes */
+$('#name').focus(function() {
+    $('#success').html('');
+});
