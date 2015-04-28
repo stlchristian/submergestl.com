d7897faef0b2725ea88f83139807f7ab98c45539
Elliot J. Voris <elliot@voris.me>
2015-04-28 16:13:39 -0500

    Issue #2: Having success submitting the data to Google
    The data is posting, and everything seems to be going well! We may
    need to work on the cross-domain stuff... or something...
    
    TODO:
    - Have user select how many registrations
    - Redirect to PayPal with that many registrations
    - Send the user a confirmation email
    - Send the admissions team a notification
    
    Signed-off-by: Elliot J. Voris <elliot@voris.me>

--- js/register_me.js
+++ js/register_me.js
@@ -1,6 +1,6 @@
 $(function() {
 
-    $("input,textarea").jqBootstrapValidation({
+    $('.registration-form').find("input,textarea").jqBootstrapValidation({
         preventSubmit: true,
         submitError: function($form, event, errors) {
             // additional error messages or events
@@ -17,33 +17,49 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "https://docs.google.com/a/voris.me/forms/d/1URNV6647mn4yDhsgAm3Yvg3mG4FE8Oxgrt2emS_prZA/formResponse",
-                type: "POST",
+                url: "https://docs.google.com/forms/d/1URNV6647mn4yDhsgAm3Yvg3mG4FE8Oxgrt2emS_prZA/formResponse",
+                method: "POST",
                 data: {
                     "entry.1239589934": name,
                     "entry.1275507538": email,
                     "entry.926689343": url
                 },
                 dataType: "xml",
-                success: function() {
-                    // Success message
-                    $('#success').html("<div class='alert alert-success'>");
-                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
-                    $('#success > .alert-success')
-                        .append('</div>');
+                //success: function() {
+                statusCode: {
+                    0: function() {
+                        // Success message
+                        $('#success').html("<div class='alert alert-success'>");
+                        $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                            .append("</button>");
+                        $('#success > .alert-success')
+                            .append("<strong>Your registration has been received. Thank you!</strong>");
+                        $('#success > .alert-success')
+                            .append('</div>');
 
-                    //clear all fields
-                    $('#registerForm').trigger("reset");
+                        //clear all fields
+                        $('#registerForm').trigger("reset");
+                    },
+                    200: function() {
+                        // Success message
+                        $('#success').html("<div class='alert alert-success'>");
+                        $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                            .append("</button>");
+                        $('#success > .alert-success')
+                            .append("<strong>Your registration has been received. Thank you!</strong>");
+                        $('#success > .alert-success')
+                            .append('</div>');
+
+                        //clear all fields
+                        $('#registerForm').trigger("reset");
+                    }
                 },
                 error: function() {
                     // Fail message
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    $('#success > .alert-danger').append("<strong>Sorry, " + firstName + ". It seems that something's not working properly. Please try again later.");
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#registerForm').trigger("reset");
@@ -63,6 +79,6 @@ $(function() {
 
 
 /*When clicking on Full hide fail/success boxes */
-$('#name').focus(function() {
+$('#reg-name').focus(function() {
     $('#success').html('');
 });
